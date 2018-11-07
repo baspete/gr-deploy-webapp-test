ce2edd818159cab80e711b0ceb2c0046177cb6d9
Pete Butler <pete.butler@ge.com>
2018-11-07 09:48:57 -0800

    updated

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express'),
   app = express();
 
 app.use('/', (req, res) => {
-  res.send('gr-deploy-webapp-test is alive!');
+  res.send("It's alive!");
 });
 
 const port = process.env.PORT || 9000;
